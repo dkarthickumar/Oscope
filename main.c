220e68e7c2bcbe0778a7569b340e102e1c13e5b1
karthic kumar <kkarthic@lab126.com>
2017-03-06 13:37:49 -0800

    Minor cleanup

--- main.c
+++ main.c
@@ -63,8 +63,8 @@ int main(void)
 	CLKDIVbits.PLLPOST=0;   				// N1 = 2  
 	CLKDIVbits.PLLPRE=0;    				// N2 = 2  
 
-//   	__builtin_write_OSCCONH(0x01);			// New Oscillator FRC w/ PLL  
-//    	__builtin_write_OSCCONL(0x01);  		// Enable Switch  
+//  __builtin_write_OSCCONH(0x01);			// New Oscillator FRC w/ PLL  
+//  __builtin_write_OSCCONL(0x01);  		// Enable Switch  
       
 	while(OSCCONbits.COSC != 0b001);		// Wait for new Oscillator to become FRC w/ PLL    
     	while(OSCCONbits.LOCK != 1);			// Wait for Pll to Lock  
